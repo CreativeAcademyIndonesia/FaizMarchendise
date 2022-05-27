723985bc547fedc4e6464e37de5580b8037ba616
CreativeAcademyIndonesia <agus.sugandi95@gmail.com>
2022-05-27 23:15:28 +0700

    link

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const navbar = document.getElementsByClassName('mynav')[0];
-console.log(navbar);
 window.addEventListener('scroll', function() {
     if (window.pageYOffset <= 0 ) {
         navbar.style.background = 'transparent';
@@ -9,3 +8,12 @@ window.addEventListener('scroll', function() {
         navbar.style.boxShadow = '0px 4px 24px rgba(105, 105, 105, 0.1)'
     }
 });
+
+
+function whatsapp() {
+    window.location.href ="https://api.whatsapp.com/send?phone=6281317214457&text=Hallo%20Saya%20tertarik%20dengan%20jasa%20dan%20produk%20anda%2C%20saya%20ingin%20order%20sesuatu";
+}
+
+function email() {
+    window.location.href = "mailto:faizmarchendise@gmail.com"
+}
